3f57b91b48884b06401567540336cc56d574dd3b
SamMCast <namelast49@gmail.com>
2020-12-02 16:34:01 -0500

    cpu: increased max instruction to 1 MILLION
    Change-Id: Ic562ff16858fe2e7a2578c0118ee690804c9c5f6

--- run_spec2006_benchmarks.sh
+++ run_spec2006_benchmarks.sh
@@ -7,8 +7,8 @@ count=0
 for bench in astar bwaves bzip2 cactusADM calculix GemsFDTD gobmk h264ref hmmer lbm leslie3d libquantum mcf milc namd omnetpp povray sjeng xalancbmk;
 do 
     echo -e "Executing bench $bench\n"
-    ./build/ARM/gem5.opt -d outoforder/$bench configs/spec2k6/run.py -b $bench\
-    --maxinsts=500000 --cpu-type=DerivO3CPU --caches --l2cache --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=8\
+    ./build/ARM/gem5.opt -d TAGE_SC_L/$bench configs/spec2k6/run.py -b $bench\
+    --maxinsts=1000000 --cpu-type=DerivO3CPU --caches --l2cache --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=8\
     --l1d_size=32kB --l1i_size=32kB --l2_size=2MB
     count=$(( $count+1 ))
 
